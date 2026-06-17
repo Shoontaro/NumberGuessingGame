4c835fbde7199f015504a2977c9b66c3b360f9bc
Shoontaro <tishinadw@gmail.com>
2026-06-17 23:46:21 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the Number Guessing Game!\n" +
+            "I'm thinking of a number between 1 and 100.\n" +
+            "You have 5 chances to guess the correct number.\n");
+
+
+    }
+}
